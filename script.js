8e7a43518e36aa249979a33cb52ccff49d9f5318
zidan <zidandewangga252@gmail.com>
2025-10-05 10:52:59 +0700

    tugas kalkulator mini

---
+++ script.js
@@ -0,0 +1,27 @@
+let A=90
+let B=15
+
+const tambah = (A,B) =>{
+    console.log(`hasil dari ${A} + ${B} adalah ${A+B}`)
+}
+tambah(A,B)
+
+const kurang = (A,B) =>{
+    console.log(`hasil dari ${A} - ${B} adalah ${A-B}`)
+}
+kurang(A,B)
+
+const kali = (A,B) =>{
+    console.log(`hasil dari ${A} x ${B} adalah ${A*B}`)
+}
+kali(A,B)
+
+const bagi = (A,B) =>{
+    console.log(`hasil dari ${A} : ${B} adalah ${A/B}`)
+}
+bagi(A,B)
+
+const modulus = (A,B) =>{
+    console.log(`hasil dari ${A} % ${B} adalah ${A%B}`)
+}
+modulus(A,B)
